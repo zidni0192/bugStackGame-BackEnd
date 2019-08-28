c9164b078446d0978cb254bbf65820d95a88ec14
zidni0192 <izedeledby@gmail.com>
2019-08-28 14:36:22 +0700

    pushing

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 
 const express = require('express')
 const app = express()
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3300
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const xss = require('x-xss-protection')
--- src/configs/db.js
+++ src/configs/db.js
@@ -1,10 +1,16 @@
 const mysql = require('mysql')
 const con = mysql.createConnection({
-    host:process.env.DB_HOST,
-    user:process.env.DB_USER,
-    password:process.env.DB_PASS,
-    database:process.env.DB_NAME
+    host:'localhost',
+    user:'root',
+    password:'',
+    database:'bugstack'
 })
+// const con = mysql.createConnection({
+//     host:process.env.DB_HOST,
+//     user:process.env.DB_USER,
+//     password:process.env.DB_PASS,
+//     database:process.env.DB_NAME
+// })
 
 con.connect((err)=>{
     if(err)console.log(err)
